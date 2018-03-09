9b7ca476ca3be6eaae9c554d40a47bf9b49f1975
work <work@163.com>
2018-03-09 16:58:57 +0800

    20180309 kx

--- time/pages/my/my.js
+++ time/pages/my/my.js
@@ -1,3 +1,6 @@
+import {My} from 'my-model.js';
+var my = new My();
+
 Page({
 
   /**
@@ -11,7 +14,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-   
+    this._loadData(); 
+  },
+
+  _loadData:function(){
+    my.getUserInfo((data) => {
+      this.setData({
+        userInfo:data
+      });
+    });
   },
 
   /**
--- time/utils/base.js
+++ time/utils/base.js
@@ -1,11 +1,13 @@
-import {Config} from '../utils/config.js';
+import {Config} from 'config.js';
+import {Token} from 'token.js';
 
 class Base{
   constructor(){
     this.baseRequestUrl = Config.restUrl;
   }
 
-  request(params){
+  request(params,noRefetch){
+    var that = this;
     var url = this.baseRequestUrl + params.url;
     if (!params.type) {
       params.type = 'GET';
@@ -19,13 +21,33 @@ class Base{
         'token':wx.getStorageSync('token')
       },
       success:function(res){
-        params.sCallBack&&params.sCallBack(res.data);
+        var code = res.statusCode.toString();
+        var startChar = code.charAt(0);
+        if(startChar == '2'){
+          params.sCallBack && params.sCallBack(res.data);
+        }else{
+          if(code == '401'){
+            if(!noRefetch){
+              that._refetch(params);
+            }
+          }
+          if(noRefetch){
+            params.eCallBack && params.eCallBack(res.data);
+          }
+        }
       },
       fail:function(err){
         console.log(err);
       }
     })
   };
+
+  _refetch(params){
+    var token = new Token();
+    token.getTokenFromServer((token)=>{
+      this.request(params,true);
+    });
+  }
 
   /*获取元素上绑定的值*/
   getDataSet(event,key){
